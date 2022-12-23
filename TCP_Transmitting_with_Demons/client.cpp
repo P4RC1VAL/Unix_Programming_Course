9df8dc12fb0d5832b9facd333ad0656094bce248
P4RC1VAL <79451855+P4RC1VAL@users.noreply.github.com>
2022-12-23 15:46:07 +0300

    Add files via upload

--- TCP_Transmitting_with_Demons/client.cpp
+++ TCP_Transmitting_with_Demons/client.cpp
@@ -7,22 +7,27 @@ int main()
 	char* buf;
 	dbg_read(buf);
 
-	while (true) {
-    if ((rv = poll(fds, 1, 100) > 0)) {
+    while (true)
+    {
+        if ((rv = poll(fds, 1, 100) > 0))
+        {
 
-        if (fds[0].revents & POLLIN ){
-            recv(sockfd, buff, 200, 0);
-            printf("From Server : %s", buff);
-            bzero(buff, sizeof(buff));
+            if (fds[0].revents & POLLIN)
+            {
+                recv(sockfd, buff, 200, 0);
+                printf("From Server : %s", buff);
+                bzero(buff, sizeof(buff));
 
-        } else if (fds[0].revents & POLLOUT){
-            puts(buff);
-            strcpy(buff, "HELLO WORLD");
-            write(sockfd, buff, sizeof(buff));
-            break;
+            }
+            else if (fds[0].revents & POLLOUT)
+            {
+                puts(buff);
+                strcpy(buff, "HELLO WORLD");
+                write(sockfd, buff, sizeof(buff));
+                break;
+            }
         }
     }
-}
 
 	dbg_close();
 }
--- TCP_Transmitting_with_Demons/lib_dbg.c
+++ TCP_Transmitting_with_Demons/lib_dbg.c
@@ -14,76 +14,89 @@
 #include <string.h>
 #include <time.h>
 
-void poller(int socketIn, std::string srcIP, int port){
- struct pollfd fds[2];
- // Initialize the fds to 0
- memset(fds, 0, sizeof(fds));
+struct pollfd fds[2];
 
- // Wait in ms before poll times out. -1 for infinite
- int timeout = 15000;
+void poller(int socketIn, std::string srcIP, int port)
+{
+	// Initialize the fds to 0
+	memset(fds, 0, sizeof(fds));
 
- fds[0].fd = socketIn;
+	// Wait in ms before poll times out. -1 for infinite
+	int timeout = 15000;
 
- while(true){
-  fds[0].events = POLLIN;
-  fds[1].fd = socketOut;
-  fds[1].events = POLLIN;
+	fds[0].fd = socketIn;
 
-  if(inWritePending){
-   fds[1].events = POLLOUT;
-   fds[0].events = 0;
-  }
+	while (true)
+	{
+		fds[0].events = POLLIN;
+		fds[1].fd = socketOut;
+		fds[1].events = POLLIN;
 
-  if(outWritePending){
-   fds[0].events = POLLOUT;
-   if(!inWritePending){
-    fds[1].events = 0;
-   }
-  }
+		if (inWritePending)
+		{
+			fds[1].events = POLLOUT;
+			fds[0].events = 0;
+		}
 
+		if (outWritePending)
+		{
+			fds[0].events = POLLOUT;
+			if (!inWritePending)
+			{
+				fds[1].events = 0;
+			}
+		}
+	}
+}
 
 int dbg_open()
 {
-  if(poll(fds, 2, timeout) > 0){
-   if(fds[0].revents & (POLLNVAL|POLLERR|POLLHUP)){
-    clientdisconnected();
-    break;
-   }
-   
-   if(fds[1].revents & (POLLNVAL|POLLERR|POLLHUP)){
-    serverdisconnected();
-    break;
-   }
+	if (poll(fds, 2, timeout) > 0)
+	{
+		if (fds[0].revents & (POLLNVAL | POLLERR | POLLHUP))
+		{
+			clientdisconnected();
+			break;
+		}
+
+		if (fds[1].revents & (POLLNVAL | POLLERR | POLLHUP))
+		{
+			serverdisconnected();
+			break;
+		}
+	}
 }
 
 
 void dbg_write(const char* msg)
 {
-	 if(fds[0].revents & POLLIN){
-    clientread();
-    break;
-   }
+	if (fds[0].revents & POLLIN)
+	{
+		clientread();
+		break;
+	}
 
-   if(fds[1].fd!=0 && fds[1].revents & POLLOUT){
-    serverwrite();
-   }
-     }
+	if (fds[1].fd != 0 && fds[1].revents & POLLOUT)
+	{
+		serverwrite();
+	}
 }
 
 void dbg_read(char* msg)
 {
-      if(fds[1].fd!=0 && fds[1].revents & POLLIN){
-    serverread();
-   }
+	if(fds[1].fd!=0 && fds[1].revents & POLLIN)
+	{
+		serverread();
+	}
 
-   if(fds[0].revents & POLLOUT){
-    clientwrite();
-    break;
-   }
+	if(fds[0].revents & POLLOUT)
+	{
+		clientwrite();
+		break;
+	}
 }
 
 void dbg_close()
 {
- closeconn();
+	closeconn();
 }
-
