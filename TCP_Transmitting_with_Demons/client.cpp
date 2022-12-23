a4c43d2cecaa6e2660fefd520f3d1f6712c78e70
P4RC1VAL <79451855+P4RC1VAL@users.noreply.github.com>
2022-12-23 14:50:42 +0300

    Update client.cpp

--- TCP_Transmitting_with_Demons/client.cpp
+++ TCP_Transmitting_with_Demons/client.cpp
@@ -7,7 +7,22 @@ int main()
 	char* buf;
 	dbg_read(buf);
 
-	printf("Client got msg [%s]", buf);
+	while (true) {
+    if ((rv = poll(fds, 1, 100) > 0)) {
+
+        if (fds[0].revents & POLLIN ){
+            recv(sockfd, buff, 200, 0);
+            printf("From Server : %s", buff);
+            bzero(buff, sizeof(buff));
+
+        } else if (fds[0].revents & POLLOUT){
+            puts(buff);
+            strcpy(buff, "HELLO WORLD");
+            write(sockfd, buff, sizeof(buff));
+            break;
+        }
+    }
+}
 
 	dbg_close();
 }
